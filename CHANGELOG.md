f30732675c5690efa7171a85864d807b3dce9198
David I. Lehn <dlehn@digitalbazaar.com>
2017-08-14 22:22:17 -0400

    Add webpack support.
    - Add webpack support and file dist rules.
    - Split messageDigest into Node.js and browser versions.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ### Added
 - RDF Dataset Normalization async implementation from [jsonld.js][].
+- webpack support.
+- Split messageDigest into Node.js and browser files.
+  - Node.js file uses native crypto module.
+  - Browser file uses forge.
 
 ## Before 0.4.0
 
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+/**
+ * rdf-canonize webpack build rules.
+ *
+ * @author Digital Bazaar, Inc.
+ *
+ * Copyright 2010-2017 Digital Bazaar, Inc.
+ */
+const path = require('path');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+
+// build multiple outputs
+module.exports = [];
+
+// custom setup for each output
+// all built files will export the "rdf-canonize" library but with different
+// content
+const outputs = [
+  // core library
+  {
+    entry: ['./index.js'],
+    filenameBase: 'rdf-canonize'
+  }
+];
+
+outputs.forEach((info) => {
+  // common to bundle and minified
+  const common = {
+    // each output uses the "jsonld" name but with different contents
+    entry: {
+      'rdf-canonize': info.entry
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      //new webpack.DefinePlugin({
+      //})
+    ],
+    // disable various node shims as jsonld handles this manually
+    node: {
+      Buffer: false,
+      process: false,
+      crypto: false,
+      setImmediate: false
+    }
+  };
+
+  // plain unoptimized unminified bundle
+  const bundle = webpackMerge(common, {
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: info.filenameBase + '.js',
+      library: info.library || '[name]',
+      libraryTarget: info.libraryTarget || 'umd'
+    }
+  });
+  if(info.library === null) {
+    delete bundle.output.library;
+  }
+  if(info.libraryTarget === null) {
+    delete bundle.output.libraryTarget;
+  }
+
+  // optimized and minified bundle
+  const minify = webpackMerge(common, {
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: info.filenameBase + '.min.js',
+      library: info.library || '[name]',
+      libraryTarget: info.libraryTarget || 'umd'
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: true
+        },
+        output: {
+          comments: false
+        }
+        //beautify: true
+      })
+    ]
+  });
+  if(info.library === null) {
+    delete minify.output.library;
+  }
+  if(info.libraryTarget === null) {
+    delete minify.output.libraryTarget;
+  }
+
+  module.exports.push(bundle);
+  module.exports.push(minify);
+});
