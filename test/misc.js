8ed10c45ce894fa764df470c1658bbb686579534
David I. Lehn <dlehn@digitalbazaar.com>
2023-05-19 02:44:40 -0400

    Add canonicalIdMap test.

--- test/misc.js
+++ test/misc.js
@@ -48,4 +48,29 @@ describe.only('API tests', () => {
     }
     assert(error);
   });
+
+  it('should return maps', async () => {
+    const input = `\
+_:b0 <urn:p0> _:b1 .
+_:b1 <urn:p1> "v1" .
+`;
+    const expected = `\
+_:c14n0 <urn:p0> _:c14n1 .
+_:c14n1 <urn:p1> "v1" .
+`;
+    const expectIdMap = new Map(Object.entries({
+      "_:b0": "_:c14n0",
+      "_:b1": "_:c14n1"
+    }));
+
+    const canonicalIdMap = new Map();
+    const output = await rdfCanonize.canonize(input, {
+      algorithm: 'URDNA2015',
+      inputFormat: 'application/n-quads',
+      format: 'application/n-quads',
+      canonicalIdMap
+    });
+    assert.deepStrictEqual(output, expected);
+    assert.deepStrictEqual(canonicalIdMap, expectIdMap);
+  });
 });
